65c8650319c31ea6f4e9baa34d5a60ab87f82b37
jovananovakovic <jovananovakovic404@gmail.com>
2018-07-06 21:36:10 +0200

    nalog za placanje

---
+++ server/src/main/java/com/project/domain/NalogZaPlacanje.java
@@ -0,0 +1,168 @@
+package com.project.domain;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class NalogZaPlacanje {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private  String duznik;
+
+    @Column(nullable = false)
+    private  String svrha;
+
+    @Column(nullable = false)
+    private  String primalac;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date datumNaloga;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date datumValute;
+
+    @Column(length = 18)
+    private  String racunDuznika;
+
+    @Column(length = 2)
+    private  Integer modelZaduzenja;
+
+    @Column(length = 20,nullable = false)
+    private  String pozivNaBrojZaduzenja;
+
+    @Column(length = 18,nullable = false)
+    private  String racunPoverioca;
+
+    @Column(length = 2,nullable = false)
+    private  Integer modelOdobrenja;
+
+    @Column(length = 2,nullable = false)
+    private  String pozivNaBrojOdobrenja;
+
+    @Column(length = 3,nullable = false)
+    private  String oznakaValute;
+
+    @Column(columnDefinition = "boolean default false")
+    private  Boolean hitno;
+
+    public NalogZaPlacanje() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDuznik() {
+        return duznik;
+    }
+
+    public void setDuznik(String duznik) {
+        this.duznik = duznik;
+    }
+
+    public String getSvrha() {
+        return svrha;
+    }
+
+    public void setSvrha(String svrha) {
+        this.svrha = svrha;
+    }
+
+    public String getPrimalac() {
+        return primalac;
+    }
+
+    public void setPrimalac(String primalac) {
+        this.primalac = primalac;
+    }
+
+    public Date getDatumNaloga() {
+        return datumNaloga;
+    }
+
+    public void setDatumNaloga(Date datumNaloga) {
+        this.datumNaloga = datumNaloga;
+    }
+
+    public Date getDatumValute() {
+        return datumValute;
+    }
+
+    public void setDatumValute(Date datumValute) {
+        this.datumValute = datumValute;
+    }
+
+    public String getRacunDuznika() {
+        return racunDuznika;
+    }
+
+    public void setRacunDuznika(String racunDuznika) {
+        this.racunDuznika = racunDuznika;
+    }
+
+    public Integer getModelZaduzenja() {
+        return modelZaduzenja;
+    }
+
+    public void setModelZaduzenja(Integer modelZaduzenja) {
+        this.modelZaduzenja = modelZaduzenja;
+    }
+
+    public String getPozivNaBrojZaduzenja() {
+        return pozivNaBrojZaduzenja;
+    }
+
+    public void setPozivNaBrojZaduzenja(String pozivNaBrojZaduzenja) {
+        this.pozivNaBrojZaduzenja = pozivNaBrojZaduzenja;
+    }
+
+    public String getRacunPoverioca() {
+        return racunPoverioca;
+    }
+
+    public void setRacunPoverioca(String racunPoverioca) {
+        this.racunPoverioca = racunPoverioca;
+    }
+
+    public Integer getModelOdobrenja() {
+        return modelOdobrenja;
+    }
+
+    public void setModelOdobrenja(Integer modelOdobrenja) {
+        this.modelOdobrenja = modelOdobrenja;
+    }
+
+    public String getPozivNaBrojOdobrenja() {
+        return pozivNaBrojOdobrenja;
+    }
+
+    public void setPozivNaBrojOdobrenja(String pozivNaBrojOdobrenja) {
+        this.pozivNaBrojOdobrenja = pozivNaBrojOdobrenja;
+    }
+
+    public String getOznakaValute() {
+        return oznakaValute;
+    }
+
+    public void setOznakaValute(String oznakaValute) {
+        this.oznakaValute = oznakaValute;
+    }
+
+    public Boolean getHitno() {
+        return hitno;
+    }
+
+    public void setHitno(Boolean hitno) {
+        this.hitno = hitno;
+    }
+}
