86c149f206ca61dcf56f95d1f6d5f5b23494d258
ivukasinovic <ivanvukasino@gmail.com>
2018-07-06 21:59:53 +0200

    added PreduzeceRepository

---
+++ server/src/main/java/com/project/repository/PreduzeceRepository.java
@@ -0,0 +1,15 @@
+package com.project.repository;
+
+import com.project.domain.Preduzece;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by Dejan Stojkic (Smek) on 7/6/2018.
+ */
+public interface PreduzeceRepository extends JpaRepository<Preduzece,Long> {
+
+    Preduzece findByEmailAndLozinka(String email, String lozinka);
+
+
+
+}
