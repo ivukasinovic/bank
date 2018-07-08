9308e546489878c094174d0e8126dee118592959
ivukasinovic <ivanvukasino@gmail.com>
2018-07-08 23:57:36 +0200

    izmene

--- server/src/main/java/com/project/endpoint/BankEndpoint.java
+++ server/src/main/java/com/project/endpoint/BankEndpoint.java
@@ -1,5 +1,6 @@
 package com.project.endpoint;
 
+import com.project.config.Converter;
 import com.project.domain.*;
 import com.project.service.*;
 import com.project.ws.*;
@@ -9,6 +10,8 @@ import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.XMLGregorianCalendar;
 import java.util.Date;
 import java.util.List;
 
@@ -73,9 +76,11 @@ public class BankEndpoint {
     public ExportNalogaResponse exportNalogaRequest(@RequestPayload ExportNalogaRequest exportNalogaRequest){
         //prosledju je mi id duznika
         ExportNalogaResponse response = new ExportNalogaResponse();
-        Preduzece preduzece = new Preduzece();
+        System.out.println("\nU servisu .. " + exportNalogaRequest.getNalog());
         //Id iz stringa uzimam id preduzeca  od kojeg uzimam naloge za placanje
         Long idDuznika= Long.valueOf(exportNalogaRequest.getNalog());
+        System.out.println("\n Broj .. " + idDuznika);
+        //  NalogZaPlacanje nalozi = nalogZaPlacanjeService.findOne(5L);
         List<NalogZaPlacanje> nalozi = nalogZaPlacanjeService.findByDuznik(idDuznika);
         //setujem respons za sve naloge za placanje
         for(NalogZaPlacanje nalogZaPlacanje: nalozi) {
@@ -90,14 +95,65 @@ public class BankEndpoint {
             nalogZaPlacanjeXML.setOznakaValute(nalogZaPlacanje.getOznakaValute());
             nalogZaPlacanjeXML.setPozivNaBrojOdobrenja(nalogZaPlacanje.getPozivNaBrojOdobrenja());
             nalogZaPlacanjeXML.setPozivNaBrojZaduzenja(nalogZaPlacanje.getPozivNaBrojZaduzenja());
+            nalogZaPlacanjeXML.setIznos(nalogZaPlacanje.getIznos());
 
-            //set jos 5 atributa
+            AdresaXML adresaProdavacXML = new AdresaXML();
+            adresaProdavacXML.setBroj(nalogZaPlacanje.getPrimalac().getAdresa().getBroj());
+            adresaProdavacXML.setDrzava(nalogZaPlacanje.getPrimalac().getAdresa().getDrzava());
+            adresaProdavacXML.setGrad(nalogZaPlacanje.getPrimalac().getAdresa().getGrad());
+            adresaProdavacXML.setId(nalogZaPlacanje.getPrimalac().getAdresa().getId());
+            adresaProdavacXML.setPostanskiBroj(nalogZaPlacanje.getPrimalac().getAdresa().getPostanskiBroj());
+            adresaProdavacXML.setUlica(nalogZaPlacanje.getPrimalac().getAdresa().getUlica());
 
+            PreduzeceXML preduzeceProdavacXML = new PreduzeceXML();
+
+            preduzeceProdavacXML.setId(nalogZaPlacanje.getPrimalac().getId());
+            preduzeceProdavacXML.setBrojRacuna(nalogZaPlacanje.getPrimalac().getBrojRacuna());
+            preduzeceProdavacXML.setNaziv(nalogZaPlacanje.getPrimalac().getNaziv());
+            preduzeceProdavacXML.setPib(nalogZaPlacanje.getPrimalac().getPib());
+            preduzeceProdavacXML.setAdresa(adresaProdavacXML);
+
+            nalogZaPlacanjeXML.setProdavac(preduzeceProdavacXML);// PRODAVAC = PRIMALAC A DUZNIK = KUPAC
+            //kraj prodavac
+            AdresaXML adresaKupacXML = new AdresaXML();
+            adresaKupacXML.setBroj(nalogZaPlacanje.getDuznik().getAdresa().getBroj());
+            adresaKupacXML.setDrzava(nalogZaPlacanje.getDuznik().getAdresa().getDrzava());
+            adresaKupacXML.setGrad(nalogZaPlacanje.getDuznik().getAdresa().getGrad());
+            adresaKupacXML.setId(nalogZaPlacanje.getDuznik().getAdresa().getId());
+            adresaKupacXML.setPostanskiBroj(nalogZaPlacanje.getDuznik().getAdresa().getPostanskiBroj());
+            adresaKupacXML.setUlica(nalogZaPlacanje.getDuznik().getAdresa().getUlica());
+
+            PreduzeceXML preduzeceKupacXML = new PreduzeceXML();
+
+            preduzeceKupacXML.setId(nalogZaPlacanje.getDuznik().getId());
+            preduzeceKupacXML.setBrojRacuna(nalogZaPlacanje.getDuznik().getBrojRacuna());
+            preduzeceKupacXML.setNaziv(nalogZaPlacanje.getDuznik().getNaziv());
+            preduzeceKupacXML.setPib(nalogZaPlacanje.getDuznik().getPib());
+            preduzeceKupacXML.setAdresa(adresaKupacXML);
+
+            nalogZaPlacanjeXML.setProdavac(preduzeceKupacXML);// PRODAVAC = PRIMALAC A DUZNIK = KUPAC
+            //kraj kupac
+
+            XMLGregorianCalendar datumNalog = null;
+            try {
+                datumNalog = Converter.vracaXmlDate(nalogZaPlacanje.getDatumNaloga());
+            } catch (DatatypeConfigurationException e) {
+                e.printStackTrace();
+            }
+            nalogZaPlacanjeXML.setDatumNaloga(datumNalog);
+            XMLGregorianCalendar datumValute = null;
+            try {
+                datumValute = Converter.vracaXmlDate(nalogZaPlacanje.getDatumValute());
+            } catch (DatatypeConfigurationException e) {
+                e.printStackTrace();
+            }
+            nalogZaPlacanjeXML.setDatumNaloga(datumValute);
+            //messageRes.setDateSent(datumDati);
 
             //  nalogZaPlacanjeXML.setDatumNaloga(nalogZaPlacanje.getDatumNaloga());
 
 
-            //response.getNalozi().add(nalogZaPlacanje);
+            response.getNalozi().add(nalogZaPlacanjeXML);
 
         }
 
